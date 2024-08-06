3df3229301813b7a38ff8e8ad7731071dcc55ce5
VladisAga <boss7551va@gmail.com>
2024-08-06 12:39:19 +0300

    feature: changed display of the screens

--- src/features/Map/ui/Map.styles.ts
+++ src/features/Map/ui/Map.styles.ts
@@ -1,19 +1,19 @@
 import { Dimensions, StyleSheet } from "react-native";
 
-const { width, height } = Dimensions.get('window');
+const { width, height } = Dimensions.get("window");
 
 const vw = (percentage: number) => {
-  return (width * percentage) / 100;
+	return (width * percentage) / 100;
 };
 
 const vh = (percentage: number) => {
-  return (height * percentage) / 100;
+	return (height * percentage) / 100;
 };
 
 export const styles = StyleSheet.create({
-    map: {
-        width: vw(100), 
-        height: vh(100),
-    },
+	map: {
+		width: vw(100), 
+		height: vh(100),
+	},
 });
 
